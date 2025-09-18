a5796b44cb3a66cfc5cba4b16eb0f77a4f31f588
[dyad] <zerlake@gmail.com>
2025-09-18 17:27:08 +0800

    [dyad] Confirming all components are implemented - wrote 1 file(s), renamed 1 file(s) + extra files edited outside of Dyad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/providers";
+import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { LandingHeader } from "@/components/landing-header";
 import { LandingFooter } from "@/components/landing-footer";
