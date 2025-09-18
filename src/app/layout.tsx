cf0deca03ec4dd045d669e7c32ee60055ae28abe
[dyad] <zerlake@gmail.com>
2025-09-18 19:22:56 +0800

    [dyad] Fixing a hydration error - wrote 1 file(s) + extra files edited outside of Dyad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
           fontSans.variable,
           fontSerif.variable
         )}
+        suppressHydrationWarning
       >
         <SkipToContentLink />
         <ThemeProvider
