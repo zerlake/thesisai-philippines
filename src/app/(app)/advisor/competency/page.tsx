be61cd112ee52cdefc5e72b05fb8738847ca2be0
[dyad] <zerlake@gmail.com>
2025-09-18 23:21:00 +0800

    [dyad] Adding a competency self-assessment for advisors - wrote 5 file(s) + extra files edited outside of Dyad

---
+++ src/app/(app)/advisor/competency/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { AdvisorCompetencyAssessment } from "@/components/advisor-competency-assessment";
+
+export default function CompetencyPage() {
+  return <AdvisorCompetencyAssessment />;
+}
