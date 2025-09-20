3735c8bee1ca1cb5b8dc124cbf1f6321c8331fa4
[dyad] <zerlake@gmail.com>
2025-09-20 21:13:51 +0800

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/root-layout-client.tsx
+++ src/components/root-layout-client.tsx
@@ -8,15 +8,7 @@ import { isPublicPage } from "@/lib/public-paths";
 export function RootLayoutClient({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   
-  if (isPublicPage(pathname)) {
-    return (
-      <div className="flex min-h-dvh flex-col">
-        <LandingHeader />
-        <main className="flex-1">{children}</main>
-        <LandingFooter />
-      </div>
-    );
-  }
-
+  // Temporarily removing conditional rendering to diagnose ChunkLoadError
+  // This will force all pages to load the main app layout structure.
   return <>{children}</>;
 }
